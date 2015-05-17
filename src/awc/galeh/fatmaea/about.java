d83439cf3f41680f40d3b866d7f1a44f9500c6ce
gfea <galeh.fatma@gmail.com>
2015-05-17 07:48:04 +0700

    Doa

---
+++ src/awc/galeh/fatmaea/about.java
@@ -0,0 +1,15 @@
+package awc.galeh.fatmaea;
+
+
+import doa.islam.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class about extends Activity {
+	   /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
---
+++ src/awc/galeh/fatmaea/help.java
@@ -0,0 +1,16 @@
+package awc.galeh.fatmaea;
+
+
+import doa.islam.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class help extends Activity {
+	   /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+
+}
